500a4ef62dcc6cea61bcae15204e65ee81fd34e1
Ilan Lallemand <ilan@MacBook-Pro-de-Ilan.local>
2024-01-29 15:42:41 +0100

    test change

--- src/components/aboutus.jsx
+++ src/components/aboutus.jsx
@@ -183,7 +183,7 @@ export default function Example() {
           </div>
           <div className="hidden lg:flex lg:flex-1 lg:justify-end">
             <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-              Log in <span aria-hidden="true">&rarr;</span>
+              connexion <span aria-hidden="true">&rarr;</span>
             </a>
           </div>
         </nav>
