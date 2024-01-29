b76f8b8852f82ec61484466914f271c2b6c26672
anthonyvouin <72597812+anthonyvouin@users.noreply.github.com>
2024-01-29 16:36:05 +0100

    create component search
    create component search

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import LandingPage from './components/landingPage';
+import searchComponent from './components/search';
 
 
 function App() {
   return (
     <Router>
-    <Switch>
-      <Route path="/" exact component={LandingPage} />
-      <Route path="/scanpage" component={LandingPage} />
-    </Switch>
-  </Router>
-
+      <Switch>
+        <Route path="/" exact component={LandingPage} />
+        <Route path="/scanpage" component={LandingPage} />
+        <Route path="/search" component={searchComponent} />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/components/search.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const searchComponent = () => {
+  return (
+    <div>
+      <h2>Ceci est un composant de test</h2>
+      <p>Bienvenue dans votre composant de test React !</p>
+    </div>
+  );
+};
+
+export default searchComponent
